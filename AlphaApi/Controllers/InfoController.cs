f7d42014ba49d24789a05d3dc9f6eef945b98f4a
Hevair Rodrigues <hevair.rodrigues@gmail.com>
2021-10-22 00:32:58 -0300

    first commit

---
+++ AlphaApi/Controllers/InfoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlphaApi.Controllers
+{
+   [ApiController]
+   [Route("api/[controller]")]
+    public class InfoController : ControllerBase
+    {
+
+        [HttpGet]
+        public object Get()
+        {
+
+            var obj = new {
+                    Name = "ApiAlpha",
+                    Description = "Api Rest Using .net core with Swagger ",
+                    Author = "Hevair Rodrigues",
+                    _type= "studing"
+                };
+
+
+            return obj;
+        }
+    }
+}
